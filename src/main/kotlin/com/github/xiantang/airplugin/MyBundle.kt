fa7e40606583ad345e434193c2b1fcc531692e08
xiantang <zhujingdi1998@gmail.com>
2020-08-22 13:02:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/xiantang/airplugin/MyBundle.kt
+++ src/main/kotlin/com/github/xiantang/airplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.xiantang.airplugin
 
 import com.intellij.AbstractBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/xiantang/airplugin/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/xiantang/airplugin/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.xiantang.airplugin.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.xiantang.airplugin.MyBundle
 
 class MyApplicationService {
 
